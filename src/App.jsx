82e5c5deefad905e6e25bd597645cf30a111e5c7
adiaz980 <141368651+adiaz980@users.noreply.github.com>
2024-01-03 15:52:37 +0000

    css file

---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { FavoritesContext } from './Favorites';
+import EntityCard from './components/EntityCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '/workspaces/ADStarwarsReadinglist/src/index.css'
+
+function App() {
+  const {store, actions} = useContext(FavoritesContext);
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const peopleRes = await fetch('https://www.swapi.tech/api/people');
+      const peopleData = await peopleRes.json();
+      setPeople(peopleData.results);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  fetchData();
+}, []);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const planetsRes = await fetch ('https://www.swapi.tech/api/planets')
+      const planetsData = await planetsRes.json();
+      setPlanets(planetsData.results);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  fetchData();
+}, []);
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const vehiclesRes = await fetch ('https://www.swapi.tech/api/vehicles')
+      const vehiclesData = await vehiclesRes.json();
+      setVehicles(vehiclesData.results);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  fetchData();
+}, []);
+
+  return (
+    <div style={{ 
+      backgroundImage: 'url("https://png.pngtree.com/png-clipart/20231006/original/pngtree-star-wars-png-free-download-png-image_13130372.png")', 
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      minHeight: '100vh',
+      padding: '20px'
+    }}>
+      <div className="characters-heading-container">
+        <div className="characters-heading">
+          <h1>Characters</h1>
+        </div>
+      </div>
+      <div className="characters-container">
+      {
+        people.map(({uid, name, url}) => {
+          return (
+            <EntityCard key={uid} data={{uid, name, url, type:"characters"}} />
+          )
+        })
+      }
+      </div>
+      <div className="characters-heading-container">
+      <div className="characters-heading">
+      <h1>Planets</h1>
+      </div>
+      </div>
+      <div className="characters-container">
+      {
+      planets.map(({ uid, name, url }) => {
+        return(
+          <EntityCard key={uid} data={{ uid, name, url, type:"planets" }} />
+          )
+        })
+      }
+      </div>
+      <div className="characters-heading-container">
+      <div className="characters-heading">
+      <h1>Vehicles</h1>
+      </div>
+      </div>
+      <div className="characters-container">
+      {vehicles.map(({ uid, name, url }) => {
+        return(
+          <EntityCard key={uid} data={{ uid, name, url, type:"vehicles" }} />
+          )
+        })
+      }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SWNav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import FavDropdown from "./FavDropdown";
+import '/workspaces/ADStarwarsReadinglist/src/index.css'
+
+function SWNav() {
+  return (
+      <Navbar className="justify-content-between">
+          <Navbar.Brand className="center-brand">
+              <Image
+                  src="https://www.pngall.com/wp-content/uploads/13/Star-Wars-Logo-Transparent.png"
+                  height="75"
+                  alt="Star Wars"
+              />
+          </Navbar.Brand>
+          <FavDropdown />
+      </Navbar>
+  )
+}
+
+export default SWNav;
